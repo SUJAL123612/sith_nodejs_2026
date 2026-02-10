b6c75dd23d7b9533b683969c3b9546b83945daa3
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2026-02-10 15:03:51 +0530

    commit

--- src/movies/create_movie.js
+++ src/movies/create_movie.js
@@ -1,8 +1,32 @@
-function main(req, res, client){
-    client.dbInstance.collection('movies').insertOne(req.body);
-    res.send("create movie sucessfully");
+function main(req, res, client) {
+  client.dbInstance.collection("movies").insertOne(req.body)
+    .then(() => {
+      return client.dbInstance.collection("movies").findOne({ name: req.body.name });
+    })
+    .then((data) => {
+      const response_obj = {
+        suceess: true,
+        data: data,
+        err: null,
+        err_id: null,
+      };
+
+      res.status(200).send(response_obj);
+      return;
+    })
+    .catch((err) => {
+      const response_obj = {
+        suceess: false,
+        data: null,
+        err: err.message,
+        err_id: 1000024,
+      };
+
+      res.status(400).send(response_obj);
+      return;
+    });
 }
 
-module.exports={
-    main : main
-}
+module.exports = {
+  main: main,
+};
